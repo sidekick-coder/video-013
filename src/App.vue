70c319fc0e6514c6b9e0b75e8b9c2185f00b9b37
Henrique <henryqueoli@hotmail.com>
2023-06-21 16:17:26 -0300

    feat: menu component

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,13 @@ import VMenu from './components/VMenu.vue'
     <div class="min-h-[300px] min-w-[500px] rounded bg-white p-5 text-center">
       <div class="flex h-[300px] w-full flex-col items-center justify-center">
         <v-menu>
-          <button class="rounded bg-teal-500 px-4 py-2 text-white">Show menu</button>
+          <template #activator="{ attrs }">
+            <button v-bind="attrs" class="rounded bg-teal-500 px-4 py-2 text-white">
+              Show menu
+            </button>
+          </template>
 
-          <ul class="mt-4 min-w-[150px] border px-4 py-2 text-left">
+          <ul class="min-w-[150px] border bg-white px-4 py-2 text-left">
             <li>My menu</li>
             <li>Item 1</li>
             <li>Item 2</li>
--- src/components/VMenu.vue
+++ src/components/VMenu.vue
@@ -1,6 +1,78 @@
 <script setup>
-// code
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+
+const show = ref(false)
+const activatorRef = ref()
+const contentRef = ref()
+const position = ref({
+  x: 0,
+  y: 0
+})
+
+const style = computed(() => ({
+  top: `${position.value.y}px`,
+  left: `${position.value.x}px`
+}))
+
+function onClick(event) {
+  let x = event.clientX
+  let y = event.clientY
+
+  const [rects] = activatorRef.value?.getClientRects() ?? []
+
+  if (rects) {
+    x = rects.x
+    y = rects.y + rects.height
+  }
+
+  position.value.x = x
+  position.value.y = y
+
+  show.value = !show.value
+}
+
+function onRef(element) {
+  activatorRef.value = element
+}
+
+function onClickDom(event) {
+  if (event.target === activatorRef.value) {
+    return
+  }
+
+  if (contentRef.value?.contains(event.target)) {
+    return
+  }
+
+  show.value = false
+}
+
+onMounted(() => {
+  document.addEventListener('click', onClickDom)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('click', onClickDom)
+})
+
+const activatorAttrs = {
+  ref: onRef,
+  onClick
+}
 </script>
 <template>
-  <slot />
+  <slot name="activator" :attrs="activatorAttrs" />
+
+  <teleport to="body">
+    <transition
+      enter-active-class="transition duration-200"
+      leave-active-class="transition duration-200"
+      enter-from-class="opacity-0 translate-y-2"
+      leave-to-class="opacity-0 translate-y-2"
+    >
+      <div ref="contentRef" v-if="show" class="fixed left-0 top-0" :style="style">
+        <slot />
+      </div>
+    </transition>
+  </teleport>
 </template>
